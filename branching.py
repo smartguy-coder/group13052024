68d721880cb22dd7b927197ee17397fefa66d9c9
vasylkartychak <w.i.k.mailua@gmail.com>
2024-06-06 21:09:18 +0300

    branching.py

---
+++ branching.py
@@ -0,0 +1,22 @@
+# True
+real_true = True
+
+if real_true:
+    print('true')
+
+# compare
+compare_result1 = 1 == 2  # False
+false = False
+
+if compare_result1:
+    print(4444444)
+
+if not compare_result1:
+    print(55566565)
+
+compare_two_false = compare_result1 == false
+print(compare_two_false)
+
+correct_false_comparison = false is compare_result1
+
+compare_result1 = 1 == 2
